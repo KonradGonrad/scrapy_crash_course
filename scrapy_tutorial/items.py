4979ff05f595c4c7979a244d65c1034e9234cb19
Konrad <257781@edu.p.lodz.pl>
2025-03-09 16:30:04 +0100

    Part 5

--- scrapy_tutorial/items.py
+++ scrapy_tutorial/items.py
@@ -10,3 +10,17 @@ class ScrapyTutorialItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookItem(scrapy.Item):
+    name = scrapy.Field()
+    url = scrapy.Field()
+    product_type = scrapy.Field()
+    price_ex_tax = scrapy.Field()
+    price_inc_tax = scrapy.Field()
+    tax = scrapy.Field()
+    avability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
--- scrapy_tutorial/pipelines.py
+++ scrapy_tutorial/pipelines.py
@@ -10,4 +10,51 @@ from itemadapter import ItemAdapter
 
 class ScrapyTutorialPipeline:
     def process_item(self, item, spider):
+        
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        price_keys = ['price', 'price_inc_tax', 'price_ex_tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+
+        availability_string = adapter.get('avability')
+        split_availability_string = availability_string.split('(')
+        if len(split_availability_string) < 2:
+            adapter['avability'] = 0
+        else:
+            avability_array = split_availability_string[1].split(' ')
+            adapter['avability'] = int(avability_array[0])
+
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == 'zero':
+            adapter['stars'] = 0
+        elif stars_text_value == 'one':
+            adapter['stars'] = 1
+        elif stars_text_value == 'two':
+            adapter['stars'] = 2
+        elif stars_text_value == 'three':
+            adapter['stars'] = 3
+        elif stars_text_value == 'four':
+            adapter['stars'] = 4
+        elif stars_text_value == 'five':
+            adapter['stars'] = 5
+
         return item
